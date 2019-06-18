7544fb6e6db0d306e623d71fbba9395b94b30ef7
jeanfern5 <jeanfern555@gmail.com>
2019-06-18 17:06:04 -0500

    Practicing basic schema and resolvers with different scalar types.

--- graphql-basics/src/index.js
+++ graphql-basics/src/index.js
@@ -1,1 +1,42 @@
-console.log("Hello GraphQL");
+import { GraphQLServer } from 'graphql-yoga';
+
+//Type definitions (schema)
+const typeDefs = `
+    type Query {
+        id: ID!
+        name: String!
+        age: Int!
+        employed: Boolean!
+        gpa: Float
+    }
+`
+
+//Resolvers
+const resolvers = {
+    Query: {
+        id() {
+            return "abc123";
+        },
+        name() {
+            return "Jeanette";
+        },
+        age() {
+            return 27;
+        },
+        employed() {
+            return false;
+        },
+        gpa() {
+            return;
+        },
+    }
+}
+
+const server = new GraphQLServer ({
+    typeDefs, // 'typeDefs: typeDefs' but since they are name the same it's shorten to 'typeDefs'
+    resolvers // similar to typeDefs
+})
+
+server.start(() => {
+    console.log("The server is up! http://localhost:4000")
+})
