2749e468bdfaaeeb17906b3fcb4b2dba87e0fc4f
jeanfern5 <jeanfern555@gmail.com>
2019-06-18 19:29:03 -0500

    Added filtering to users and posts.

--- graphql-basics/src/index.js
+++ graphql-basics/src/index.js
@@ -1,33 +1,112 @@
 import { GraphQLServer } from 'graphql-yoga';
 
+//Demo user data
+const users = [
+    {
+        id: "1",
+        name: "Jeanette",
+        email: "jeanette@example.com"
+    },
+    {
+        id : "2",
+        name: "Matt",
+        email: "matt@example.com"
+    },
+    {
+        id: "3",
+        name: "Taco",
+        email: "taco@example.com",
+        age: 100
+    }
+]
+
+//Demo post data
+const posts = [
+    {
+        id: "a1",
+        title: "Hello World",
+        body: "Hello World",
+        published: true
+    },
+    {
+        id: "b2",
+        title: "GraphQL",
+        body: "This is how you graphql....",
+        published: false
+    },
+    {
+        id: "c3",
+        title: "The Final Post",
+        body: "It was never completed....",
+        published: false
+    }
+]
+
 //Type definitions (schema)
 const typeDefs = `
     type Query {
+        users(query: String): [User!]!
+        me: User!
+        posts(query: String): [Post!]!
+        post: Post!
+    }
+    type User {
         id: ID!
         name: String!
-        age: Int!
-        employed: Boolean!
-        gpa: Float
+        email: String!
+        age: Int
+    }
+
+    type Post{
+       id: ID!
+       title: String!
+       body: String!
+       published: Boolean! 
     }
 `
 
 //Resolvers
 const resolvers = {
     Query: {
-        id() {
-            return "abc123";
+        users(parent, args, ctx, info) {
+            const query = args.query;
+
+            if (!query){
+                return users;
+            }
+
+            return users.filter((user) => {
+                return user.name.toLowerCase().includes(query.toLowerCase());
+            });
         },
-        name() {
-            return "Matt";
+        me() {
+            return {
+                id: "1234098",
+                name: "Mike",
+                email: "Mike@example.com"
+            };
         },
-        age() {
-            return 27;
+        posts(parent, args, ctx, info) {
+            const query = args.query;
+
+            if (!query){
+                return posts;
+            }
+
+            return posts.filter((post) => {
+                const isTitleMatch = post.title.toLowerCase().includes(query.toLowerCase());
+                const isBodyMatch = post.body.toLowerCase().includes(query.toLowerCase());
+
+                return isTitleMatch || isBodyMatch; 
+            });
         },
-        employed() {
-            return false;
-        },
-        gpa() {
-            return;
+        post() {
+            return {
+                id: "abc1234",
+                title: "GraphQL",
+                body: "This is how you graphql....",
+                published: false
+            };
         },
     }
 }
@@ -38,5 +117,5 @@ const server = new GraphQLServer ({
 })
 
 server.start(() => {
-    console.log("The server is up! http://localhost:4000")
+    console.log("\n<---------- The server is up! http://localhost:4000 ---------->\n");
 })
