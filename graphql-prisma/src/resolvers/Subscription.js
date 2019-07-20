b83c912e40b99ded4ebc45ba4d6952bd3e343b56
jeanfern5 <jeanfern555@gmail.com>
2019-07-20 17:57:31 -0500

    migrated subscriptions for comments and posts to use prisma.

--- graphql-prisma/src/resolvers/Subscription.js
+++ graphql-prisma/src/resolvers/Subscription.js
@@ -1,18 +1,12 @@
 const Subscription = {
     comment: {
-        subscribe(parent, { postId } , { db: {posts}, pubsub }, info) {
-            const post = posts.find((post) => (post.id = postId) && post.published);
-
-            if (!post) {
-                throw new Error('Post not found')
-            }
-
-            return pubsub.asyncIterator(`comment ${postId}`)
+        subscribe(parent, { postId } , { prisma }, info) {
+            return prisma.subscription.comment({ where: { node: { post: { id: postId } }  } }, info)
         }
     },
     post : {
-        subscribe(parent, args, { db: {posts}, pubsub }, info) {
-            return pubsub.asyncIterator(`post`);
+        subscribe(parent, args, { prisma }, info) {
+            return prisma.subscription.post({ where: { node: { published: true } } }, info);
         }
     }
 }
